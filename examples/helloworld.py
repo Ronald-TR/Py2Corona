adb413a00458389de46f01ff3b2dab36b9fd6a17
Ronald Rodrigues <ronald@codemed.com.br>
2018-04-26 18:40:12 -0300

    garbage tests

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -1,11 +1,12 @@
 from Py2Corona.Classes import display, Text, Button
 from Py2Corona.Consts import CENTER_Y
 
-display.path = r'C:\Projects\py2corona\main.lua' # replace with the main.lua path created by your Corona SDK project
-hello = Text('hello', 'Hello World, Py2Corona!')
+display.path = r'C:\Users\PROG-01\Documents\Corona Projects\py2corona\main.lua' # replace with the main.lua path created by your Corona SDK project
+hello = Text(text='Hello World, Py2Corona!')
 btn = Button(label='My button', font='FRSCRIPT.ttf', fontSize=35)
 btn.left = 60
 btn.top = CENTER_Y + 20
+
 display.add(hello)
 display.add(btn)
 display.compile()
