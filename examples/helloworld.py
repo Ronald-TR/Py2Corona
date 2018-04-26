7b3cc59a1e474f3f69a87acea27b5e7cf58e4935
Ronald Rodrigues <ronald@codemed.com.br>
2018-04-26 18:03:16 -0300

    path changed for uses

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -1,9 +1,9 @@
 from Py2Corona.Classes import display, Text, Button
 from Py2Corona.Consts import CENTER_Y
 
-display.path = r'C:\py2Corona\main.lua' # replace with the main.lua path created by your Corona SDK project
+display.path = r'C:\Projects\py2corona\main.lua' # replace with the main.lua path created by your Corona SDK project
 hello = Text('hello', 'Hello World, Py2Corona!')
-btn = Button('btn', 'My button')
+btn = Button(label='My button', font='FRSCRIPT.ttf', fontSize=35)
 btn.left = 60
 btn.top = CENTER_Y + 20
 display.add(hello)
